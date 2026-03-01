dad7a1a792a873e1969152076368c5d611f5b996
MomsDeveloper <georgiy.khabner@mail.ru>
2026-03-01 04:53:06 +0300

    impl positional embeder

--- embedings.py
+++ embedings.py
@@ -11,6 +11,14 @@ class TokenEmbeddings(nn.Module):
     def forward(self, x: torch.Tensor):
         return self.emb_matrix(x)
 
-model = TokenEmbeddings(700, 4)
-x = torch.tensor([[113, 456, 76, 345],
-        [345, 678, 454, 546]])
+
+class PositionalEmbeddings(nn.Module):
+    def __init__(self, max_seq_len: int, emb_size: int):
+        super().__init__()
+        self.max_seq_len = max_seq_len
+        self.emb_size = emb_size
+        self.emb_matrix = nn.Embedding(self.max_seq_len, self.emb_size)
+    
+    def forward(self, seq_len: int):
+        positions = torch.arange(seq_len)
+        return self.emb_matrix(positions)
