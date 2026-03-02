819a828cd0bc9b77fd403db68154c9c4e76bb90e
MomsDeveloper <georgiy.khabner@mail.ru>
2026-03-02 03:06:19 +0300

    add load/ save to the model

--- gpt.py
+++ gpt.py
@@ -6,7 +6,13 @@ from decoder import Decoder
 class GPT(nn.Module):
     def __init__(self, vocab_size: int, max_seq_len: int, emb_size: int, num_heads: int,  head_size: int, num_layers: int, dropout: float = 0.1, device: str = 'cpu'):
         super().__init__()
+        self.vocab_size = vocab_size
         self.max_seq_len = max_seq_len
+        self.emb_size = emb_size
+        self.num_heads = num_heads
+        self.head_size = head_size
+        self.num_layers = num_layers
+
         self.token_emb = TokenEmbeddings(vocab_size, emb_size)
         self.pos_emb = PositionalEmbeddings(max_seq_len, emb_size)
         self.dropout = nn.Dropout(dropout)
@@ -33,6 +39,32 @@ class GPT(nn.Module):
             next_token = torch.argmax(last_vector, dim=-1, keepdim=True)
             x = torch.cat([x, next_token], dim=1)
         return x
+
+    def save(self, path):
+        torch.save({
+            'model_state_dict': self.state_dict(),
+            'vocab_size': self.vocab_size,
+            'max_seq_len': self.max_seq_len,
+            'emb_size': self.emb_size,
+            'num_heads': self.num_heads,
+            'head_size': self.head_size,
+            'num_layers': self.num_layers
+        }, path)
+    
+    @classmethod
+    def load(cls, path, device):
+        checkpoint = torch.load(path, map_location=device)
+        model = cls(
+            vocab_size=checkpoint['vocab_size'],
+            max_seq_len=checkpoint['max_seq_len'],
+            emb_size=checkpoint['emb_size'],
+            num_heads=checkpoint['num_heads'],
+            head_size=checkpoint['head_size'],
+            num_layers=checkpoint['num_layers']
+        )
+        model.load_state_dict(checkpoint['model_state_dict'])
+        model.to(device)
+        return model
 
 # Example parameters
 vocab_size = 100
