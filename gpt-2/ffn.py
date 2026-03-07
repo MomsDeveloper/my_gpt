ffa774fe1e460ee21a274474bcdeb04750d12f28
MomsDeveloper <georgiy.khabner@mail.ru>
2026-03-08 01:52:29 +0300

    injected gelu in the ffn

--- gpt-2/ffn.py
+++ gpt-2/ffn.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn as nn
+from gelu import GELU
 
 class FeedForward(nn.Module):
     def __init__(self, emb_size: int, dropout: float = 0.1):
         super().__init__()
         self.ffn = nn.Sequential(
             nn.Linear(emb_size, 4 * emb_size),
-            nn.ReLU(inplace=True),
+            GELU(),
             nn.Linear(4 * emb_size, emb_size),
             nn.Dropout(dropout)
         )
